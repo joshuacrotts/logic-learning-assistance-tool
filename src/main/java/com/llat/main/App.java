4b2d140e3a744279facde49ec3a232d7a8f635c8
codingtheories <codingtheories@gmail.com>
2021-01-23 21:56:30 -0500

    adding the sample view\

--- src/main/java/com/llat/main/App.java
+++ src/main/java/com/llat/main/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("/fxml/primary"));
+        scene = new Scene(loadFXML("/fxml/sample"));
         stage.setScene(scene);
         stage.show();
     }
