25af41fa6b3caf4b7d474964b53f80b2b06861ba
codingtheories <codingtheories@gmail.com>
2021-04-02 21:09:53 -0400

    adding the register View

--- src/main/java/com/llat/controller/Controller.java
+++ src/main/java/com/llat/controller/Controller.java
@@ -3,7 +3,7 @@ package com.llat.controller;
 import com.llat.input.interpreters.LLATParserInterpreter;
 import com.llat.tools.EventBus;
 import com.llat.tools.ViewManager;
-import com.llat.views.LoginView;
+import com.llat.views.RegisterView;
 import com.llat.views.SymbolButton;
 import com.llat.views.events.SolveButtonEvent;
 import com.llat.views.events.SymbolDescriptionEvent;
@@ -56,11 +56,11 @@ public class Controller implements Initializable {
         Pane parentPane = new Pane();
         switch (_viewName) {
             case ViewManager.MAINAPPLICATION: {
-                parentPane = (new LoginView(this).getParentPane());
+                parentPane = (new RegisterView(this).getParentPane());
                 break;
             }
             case ViewManager.LOGIN:
-                parentPane = (new LoginView(this)).getParentPane();
+                parentPane = (new RegisterView(this)).getParentPane();
                 break;
             default:
                 // Update this to error view.
--- src/main/java/com/llat/views/RegisterView.java
+++ src/main/java/com/llat/views/RegisterView.java
@@ -16,101 +16,133 @@ import javafx.stage.Stage;
 public class RegisterView {
     Region aboveLogoRegion = new Region();
     Region belowLogoRegion = new Region();
-    Region belowEmailRegion = new Region();
+    Region belowUserNameRegion = new Region();
+    Region belowFirstNameRegion = new Region();
+    Region belowLastNameRegion = new Region();
     Region belowPasswordFieldRegion = new Region();
     Region belowLoginButtonRegion = new Region();
     private Controller controller;
     private AnchorPane parentPane = new AnchorPane();
     private Stage stage;
-    private VBox loginVBox = new VBox();
+    private VBox registerVBox = new VBox();
     private Label logoImage = new Label();
-    private Label emailInputLabel = new Label("Email");
-    private TextField emailField = new TextField();
+    private Label userNameInputLabel = new Label("User Name");
+    private TextField userNameField = new TextField();
+    private Label firstNameInputLabel = new Label("First Name");
+    private TextField firstNameField = new TextField();
+    private Label lastNameInputLabel = new Label("Last Name");
+    private TextField lastNameField = new TextField();
     private Label passwordInputLabel = new Label("Password");
     private PasswordField passwordField = new PasswordField();
-    private Button loginButton = new Button("Login");
+    private Button registerButton = new Button("Register");
     private Button returnButton = new Button("Return to Application");
 
     public RegisterView(Controller _controller) {
         this.controller = _controller;
         this.stage = _controller.getStage();
         // Setting VBox loginVBox properties.
-        this.loginVBox.setId("loginVBox");
+        this.registerVBox.setId("registerVBox");
         this.stage.widthProperty().addListener((obs, oldVal, newVal) -> {
-            this.loginVBox.setMinWidth(newVal.doubleValue() * .30);
+            this.registerVBox.setMinWidth(newVal.doubleValue() * .30);
         });
         this.stage.heightProperty().addListener((obs, oldVal, newVal) -> {
-            this.loginVBox.setMinHeight(newVal.doubleValue());
+            this.registerVBox.setMinHeight(newVal.doubleValue());
         });
         this.stage.widthProperty().addListener((obs, oldVal, newVal) -> {
-            this.loginVBox.setLayoutX((newVal.doubleValue() * .50) - this.loginVBox.getMinWidth() / 2);
+            this.registerVBox.setLayoutX((newVal.doubleValue() * .50) - this.registerVBox.getMinWidth() / 2);
         });
         this.stage.heightProperty().addListener((obs, oldVal, newVal) -> {
-            this.loginVBox.setLayoutY((newVal.doubleValue() * .50) - this.loginVBox.getMinHeight() / 2);
+            this.registerVBox.setLayoutY((newVal.doubleValue() * .50) - this.registerVBox.getMinHeight() / 2);
         });
         // Setting Region aboveLogoRegion properties.
-        this.loginVBox.heightProperty().addListener((obs, oldVal, newVal) -> {
+        this.registerVBox.heightProperty().addListener((obs, oldVal, newVal) -> {
             this.aboveLogoRegion.setMinHeight(newVal.doubleValue() * .05);
         });
         // Setting Label logoImage properties.
-        this.logoImage.setId("loginLogoLabel");
-        this.loginVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
-            if (this.loginVBox.getHeight() > this.loginVBox.getWidth()) {
+        this.logoImage.setId("registerLogoLabel");
+        this.registerVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
+            if (this.registerVBox.getHeight() > this.registerVBox.getWidth()) {
                 this.logoImage.setMinWidth(newVal.doubleValue() * .40);
                 this.logoImage.setMinHeight(newVal.doubleValue() * .40);
             } else {
-                this.logoImage.setMinWidth(this.loginVBox.getHeight() * .40);
-                this.logoImage.setMinHeight(this.loginVBox.getHeight() * .40);
+                this.logoImage.setMinWidth(this.registerVBox.getHeight() * .40);
+                this.logoImage.setMinHeight(this.registerVBox.getHeight() * .40);
             }
         });
 
-        this.loginVBox.heightProperty().addListener((obs, oldVal, newVal) -> {
-            if (this.loginVBox.getHeight() > this.loginVBox.getWidth()) {
-                this.logoImage.setMinWidth(this.loginVBox.getWidth() * .40);
-                this.logoImage.setMinHeight(this.loginVBox.getWidth() * .40);
+        this.registerVBox.heightProperty().addListener((obs, oldVal, newVal) -> {
+            if (this.registerVBox.getHeight() > this.registerVBox.getWidth()) {
+                this.logoImage.setMinWidth(this.registerVBox.getWidth() * .40);
+                this.logoImage.setMinHeight(this.registerVBox.getWidth() * .40);
             } else {
                 this.logoImage.setMinWidth(newVal.doubleValue() * .40);
                 this.logoImage.setMinHeight(newVal.doubleValue() * .40);
             }
         });
         // Setting Region belowLogoRegion properties.
-        this.loginVBox.heightProperty().addListener((obs, oldVal, newVal) -> {
-            this.belowLogoRegion.setMinHeight(newVal.doubleValue() * .05);
+        this.registerVBox.heightProperty().addListener((obs, oldVal, newVal) -> {
+            this.belowLogoRegion.setMinHeight(newVal.doubleValue() * .01);
         });
-        // Setting Label emailInputLabel properties.
-        this.emailInputLabel.setId("emailInputLabel");
-        // Setting TextField emailField properties.
-        this.emailField.setPromptText("Enter your email.");
-        this.emailField.setFocusTraversable(false);
-        this.loginVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
-            this.emailField.setMaxWidth(newVal.doubleValue() * .70);
+
+        // Setting Label userNameInputLabel properties.
+        this.userNameInputLabel.setId("userNameInputLabel");
+        // Setting TextField userNameField properties.
+        this.userNameField.setPromptText("Enter your User Name.");
+        this.userNameField.setFocusTraversable(false);
+        this.registerVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
+            this.userNameField.setMaxWidth(newVal.doubleValue() * .70);
         });
+
         // Setting Region belowEmailRegion properties.
-        this.loginVBox.heightProperty().addListener(((obs, oldVal, newVal) -> {
-            this.belowEmailRegion.setMinHeight(newVal.doubleValue() * .10);
+        this.registerVBox.heightProperty().addListener(((obs, oldVal, newVal) -> {
+            this.belowUserNameRegion.setMinHeight(newVal.doubleValue() * .01);
+        }));
+
+        // Setting Label firstNameInputLabel properties.
+        this.firstNameInputLabel.setId("firstNameInputLabel");
+        // Setting TextField firstNameInputField properties.
+        this.firstNameField.setPromptText("Enter your first name.");
+        this.firstNameField.setFocusTraversable(false);
+        this.registerVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
+            this.firstNameField.setMaxWidth(newVal.doubleValue() * .70);
+        });
+
+        // Setting Label lastNameInputLabel properties.
+        this.lastNameInputLabel.setId("lastNameInputLabel");
+        // Setting TextField lastNameInputField properties.
+        this.lastNameField.setPromptText("Enter your last name.");
+        this.lastNameField.setFocusTraversable(false);
+        this.registerVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
+            this.lastNameField.setMaxWidth(newVal.doubleValue() * .70);
+        });
+
+
+        // Setting Region belowEmailRegion properties.
+        this.registerVBox.heightProperty().addListener(((obs, oldVal, newVal) -> {
+            this.belowLastNameRegion.setMinHeight(newVal.doubleValue() * .01);
         }));
         // Setting Label passwordInputLabel properties.
         this.passwordInputLabel.setId("passwordInputLabel");
         // Setting PasswordField passwordField properties.
         this.passwordField.setPromptText("Enter your password.");
         this.passwordField.setFocusTraversable(false);
-        this.loginVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
+        this.registerVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
             this.passwordField.setMaxWidth(newVal.doubleValue() * .70);
         });
         // Setting Region belowPasswordFieldRegion properties.
-        this.loginVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
-            this.belowPasswordFieldRegion.setMinHeight(newVal.doubleValue() * .10);
+        this.registerVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
+            this.belowPasswordFieldRegion.setMinHeight(newVal.doubleValue() * .05);
         });
         // Setting Button loginButton properties.
-        this.loginButton.setId("loginButton");
-        this.loginVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
-            this.loginButton.setMinWidth(newVal.doubleValue() * .30);
+        this.registerButton.setId("registerButton");
+        this.registerVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
+            this.registerButton.setMinWidth(newVal.doubleValue() * .30);
         });
 
 
-        this.loginButton.setOnAction((event1 -> {
+        this.registerButton.setOnAction((event1 -> {
             DatabaseAdapter ad = new DatabaseAdapter();
-            String email = this.emailField.getText();
+            String email = this.userNameField.getText();
             String pass = this.passwordField.getText();
             UserObject user = ad.Login(email, pass);
             if(!(user == null)){
@@ -120,19 +152,19 @@ public class RegisterView {
             }
         }));
         // Setting Region belowLoginButtonRegion properties.
-        this.loginVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
-            this.belowLoginButtonRegion.setMinHeight(newVal.doubleValue() * .10);
+        this.registerVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
+            this.belowLoginButtonRegion.setMinHeight(newVal.doubleValue() * 0.1);
         });
         // Setting Button returnButton properties.
         this.returnButton.setId("returnButton");
-        this.loginVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
+        this.registerVBox.widthProperty().addListener((obs, oldVal, newVal) -> {
             this.returnButton.setMinWidth(newVal.doubleValue() * .30);
         });
-        this.loginVBox.getChildren().addAll(this.aboveLogoRegion, this.logoImage, belowLogoRegion, this.emailInputLabel, this.emailField, this.belowEmailRegion, this.passwordInputLabel, this.passwordField, this.belowPasswordFieldRegion, this.loginButton, this.belowLoginButtonRegion, this.returnButton);
+        this.registerVBox.getChildren().addAll(this.aboveLogoRegion, this.logoImage, belowLogoRegion, this.userNameInputLabel, this.userNameField, this.belowUserNameRegion, this.firstNameInputLabel, this.firstNameField, this.belowFirstNameRegion, this.lastNameInputLabel, this.lastNameField, this.belowLastNameRegion,this.passwordInputLabel, this.passwordField, this.belowPasswordFieldRegion, this.registerButton, this.belowLoginButtonRegion, this.returnButton);
         // Setting AnchorPane parentPane properties.
-        this.parentPane.setId("loginViewParentPane");
+        this.parentPane.setId("registerViewParentPane");
         // Adding children nodes to their parents nodes.
-        this.parentPane.getChildren().addAll(this.loginVBox);
+        this.parentPane.getChildren().addAll(this.registerVBox);
     }
 
     public Pane getParentPane() {
