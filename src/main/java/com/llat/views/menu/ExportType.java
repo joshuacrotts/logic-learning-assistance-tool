fe3033a66dc9ee6dd266faeffd574936213ea8dc
Joshua Crotts <joshuacrotts@yahoo.com>
2021-04-19 20:34:06 -0400

    Fixed ridiculous error 'parse table'...

--- src/main/java/com/llat/views/menu/ExportType.java
+++ src/main/java/com/llat/views/menu/ExportType.java
@@ -5,10 +5,10 @@ import javafx.stage.FileChooser;
 public enum ExportType {
 
     LATEX_TRUTH_TABLE("Latex Truth Table", ".tex", new FileChooser.ExtensionFilter("LaTeX files (*.tex)", "*.tex")),
-    LATEX_PARSE_TREE("Latex Parse Table", ".tex", new FileChooser.ExtensionFilter("LaTeX files (*.tex)", "*.tex")),
+    LATEX_PARSE_TREE("Latex Parse Tree", ".tex", new FileChooser.ExtensionFilter("LaTeX files (*.tex)", "*.tex")),
     LATEX_TRUTH_TREE("Latex Truth Tree", ".tex", new FileChooser.ExtensionFilter("LaTeX files (*.tex)", "*.tex")),
     PDF_TRUTH_TABLE("PDF Truth Table", ".pdf", new FileChooser.ExtensionFilter("PDF files (*.pdf)", "*.pdf")),
-    PDF_PARSE_TREE("PDF Parse Table", ".pdf", new FileChooser.ExtensionFilter("PDF files (*.pdf)", "*.pdf")),
+    PDF_PARSE_TREE("PDF Parse Tree", ".pdf", new FileChooser.ExtensionFilter("PDF files (*.pdf)", "*.pdf")),
     PDF_TRUTH_TREE("PDF Truth Tree", ".pdf", new FileChooser.ExtensionFilter("PDF files (*.pdf)", "*.pdf"));
 
     private final String exportType;
